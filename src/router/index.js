2952e37b7e13d5ac835ee2a4aa71cc08e2139529
jmarti <jmarti@theinit.com>
2018-05-13 12:03:38 +0200

    remove keypad2

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import HomeView from '../views/HomeView.vue'
 import BriefingsView from '../views/BreefingsView.vue'
 import CountdownView from '../views/CountdownView.vue'
 import Keypad1View from '../views/Keypad1View.vue'
-import Keypad2View from '../views/Keypad2View.vue'
 import Keypad3View from '../views/Keypad3View.vue'
 
 Vue.use(Router)
@@ -27,9 +26,6 @@ export default new Router({
   }, {
     path: '/keypad1',
     component: Keypad1View
-  }, {
-    path: '/keypad2',
-    component: Keypad2View
   }, {
     path: '/keypad3',
     component: Keypad3View
