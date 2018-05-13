71296bc1b21c8b9771aecddd7362af716957be48
jmarti <jmarti@theinit.com>
2018-05-13 12:03:53 +0200

    translate texts to spanish (missions are in spanish)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="home-view">
-    <h1>Welcome to Katayuno Escape Edition</h1>
-    <p><strong>How to play: </strong></p>
-    <p> - To start the experience, open following pages in different tabs:</p>
+    <h1>Bienvenido al Katayuno Escapecraft Edition</h1>
+    <p><strong>Cómo jugar: </strong></p>
+    <p> - Para comenzar la experiencia, abre las siguientes páginas en diferentes pestañas:</p>
     <ul>
-      <li><a href="briefings" target="_blank">The mission briefings</a></li>
-      <li><a href="countdown" target="_blank">The 3 hour countdown timer</a></li>
-      <li><a href="keypad1" target="_blank">The keypad Room 1</a></li>
-      <li><a href="keypad2" target="_blank">The keypad Room 2</a></li>
-      <li><a href="keypad3" target="_blank">The kataboom! keypad</a></li>
+      <li><a href="briefings" target="_blank">Las misiones</a></li>
+      <li><a href="countdown" target="_blank">El contador de 3 horas</a></li>
+      <li><a href="keypad1" target="_blank">El teclado de la misión 1</a></li>
+      <li><a href="keypad3" target="_blank">El teclado kataboom!</a></li>
     </ul>
-    <p> - Open the envelope with the MISSION BRIEFING <b>P</b> AND <b>NUMBER 1</b>.</p>
-    <p> - Read it carefully, and make sure you know what to do. Breathe deeply.</p>
-    <p> - Go to the countdown timer tab, and press START. Do not stop it unless it's an emergency.</p>
-    <p> - Follow instructions of the mission during the following 3 hours. Good luck, Agent!</p>
+    <p> - Abre el sobre indicado como <b>NÚMERO 1</b>.</p>
+    <p> - Léelo cuidadosamente y asegúrate que sabes qué hacer. Respira profundamente.</p>
+    <p> - Ve al contador y pulsa START. No lo detengas a no ser que sea una emergencia.</p>
+    <p> - Sigue las instrucciones de la misión durante las siguientes 3 horas. ¡Buena suerte, agente!</p>
+    <hr>
+    <p class="subline">By <a href="https://twitter.com/gonzalo123" target="_blank">@gonzalo123</a> and <a href="https://twitter.com/itortv" target="_blank">@itortv</a>.
+      Thanks to <a href="https://twitter.com/ojoven" target="_blank">@ojoven</a> and <a href="https://twitter.com/540info" target="_blank">@540info</a></p>
   </div>
 </template>
 <style>
@@ -36,12 +38,24 @@
     font-family: 'VT323', serif;
   }
 
+  .home-view hr {
+    margin-top: 75px;
+    border-color: #444;
+    color: #444;
+  }
+
+  .home-view .subline {
+    color: #444;
+  }
   @media screen and (orientation:portrait) {
     .home-view h1 {
       font-size: 4vh;
     }
     .home-view a, .home-view p {
       font-size: 3vh;
+    }
+    .home-view .subline, .home-view .subline a {
+      font-size: 2vh;
     }
 
   }
@@ -51,6 +65,9 @@
     }
     .home-view a, .home-view p {
       font-size: 2vw;
+    }
+    .home-view .subline, .home-view .subline a {
+      font-size: 1.5vw;
     }
 
   }
