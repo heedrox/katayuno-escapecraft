0efe487c773beacbf28dfd16b48ff2eb8679f5b0
jmarti <jmarti@theinit.com>
2018-05-13 13:27:40 +0200

    add the end

--- src/views/Keypad3View.vue
+++ src/views/Keypad3View.vue
@@ -1,25 +1,75 @@
 <template>
   <escape-keypad :text-done="textDone" :relative-keyboard-coords="[17,18,65,73]" skin="/static/keypad-skins/keypad3.png"
-                 class="main-keypad3" combination-hash="$2a$10$5AHlWMVICV7OQlrbz/3OQegonQBWNLddgaX9gtdYLE4XysE1YSNlS"
-                 :number-of-digits="3"></escape-keypad>
+                 class="main-keypad3" combination-hash="$2a$04$xeJX0bMdGgMWfhMX.x.Im.xNTB/JDtipRqHuzhHixXJKWHEnRg/pm"
+                 :number-of-digits="4"></escape-keypad>
 </template>
 <style>
   .main-keypad3 {
     width: 100%;
   }
-  .main-keypad3 .under-door p {
-    padding-top: 10vh;
-    font-size: 6vw;
-  }
-  .main-keypad3 .keypad input {
-    position:relative;
 
-    background-color:#000;
-    color:#fff;
+
+  .main-keypad3 .keypad input {
+    position: relative;
+    background-color: #000;
+    color: #fff;
     border: solid 1px #fff;
   }
+
   .main-keypad3 .keypad img {
-    max-width:100%;
+    max-width: 100%;
+  }
+
+  .overpose {
+    position:relative;
+    left: -220px;
+    -webkit-animation: flickerAnimation 1s infinite;
+    -moz-animation: flickerAnimation 1s infinite;
+    -o-animation: flickerAnimation 1s infinite;
+    animation: flickerAnimation 1s infinite;
+  }
+
+  .imagesEnd {
+    width: 249px !important;
+    margin: auto;
+    white-space:nowrap;
+  }
+
+  @keyframes flickerAnimation {
+    0%   { opacity:1; }
+    50%  { opacity:0; }
+    100% { opacity:1; }
+  }
+  @-o-keyframes flickerAnimation{
+    0%   { opacity:1; }
+    50%  { opacity:0; }
+    100% { opacity:1; }
+  }
+  @-moz-keyframes flickerAnimation{
+    0%   { opacity:1; }
+    50%  { opacity:0; }
+    100% { opacity:1; }
+  }
+  @-webkit-keyframes flickerAnimation{
+    0%   { opacity:1; }
+    50%  { opacity:0; }
+    100% { opacity:1; }
+  }
+
+
+  @media screen and (orientation:portrait) {
+    .main-keypad3 .under-door p {
+      font-size: 6vw;
+    }
+  }
+
+  @media screen and (orientation:landscape) {
+    .main-keypad3 .under-door p {
+      font-size: 6vh;
+      width: 80%;
+      margin-left: auto;
+      margin-right: auto;
+    }
   }
 </style>
 <script>
@@ -31,7 +81,10 @@
     },
     data () {
       return {
-        textDone: '<p>¡Enhorabuena!</p><p>¡Has terminado el Katayuno Escape Edition!</p><p>¡Y has evitado una catástrofe gracias a tus habilidades!</p><p><b>¡Felicidades!</b></p>'
+        textDone: '<p>¡Enhorabuena!</p>' +
+        '<p>¡Habéis evitado una catástrofe gracias a vuestras habilidades! Además,  el código ha redirigido el misil hacia el líder Parayerista, eliminando la amenaza para siempre.</p>' +
+        '<p class="imagesEnd"><img src="/static/the-end/lo-malo.jpg"/><img src="/static/the-end/bomb.png" class="overpose"/></p>' +
+        '<p><b>¡Felicidades!</b></p>'
       }
     }
   }
